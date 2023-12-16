297290b349e43f0e8f9dc4d4e1a2a59ae6e4d6f5
Kxxle <64878567+Kxxle@users.noreply.github.com>
2023-12-16 10:41:03 +0100

    I have added a image

--- src/main/java/vistulastudent/myfirstspringapp/controller/HelloController.java
+++ src/main/java/vistulastudent/myfirstspringapp/controller/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 //@RestController
@@ -15,7 +15,7 @@ public class HelloController {
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required = false, defaultValue ="World") String name, Model model){
-        model.addAllAttributes("name" , name);
+        model.addAttribute("name" , name);
         return "greeting";
     }
 
